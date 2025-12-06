de73a7881c707ef0675fa5695fd00b7bf4b572ef
albinjerkers <albinjerkers@gmail.com>
2025-12-06 18:40:07 +0100

    Add logger

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width {SCREEN_WIDTH}")
-    print(f"Screen height {SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
