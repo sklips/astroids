060b4093dcddf5083085e2cd87290ee0b243ab42
albinjerkers <albinjerkers@gmail.com>
2025-12-06 19:04:20 +0100

    Added player

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+
+from player import Player
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 
@@ -9,12 +11,15 @@ def main():
     game_clock = pygame.time.Clock()
     dt = 0
 
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         dt = game_clock.tick(60) / 1000
