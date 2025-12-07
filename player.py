72d9d93d6dea40a9a3ce766ef6289121ca9ba9f4
albin jerkers <albinjerkers@gmail.com>
2025-12-07 19:16:34 +0100

    Added collision with asteroids and bullets

--- player.py
+++ player.py
@@ -1,7 +1,7 @@
 # noinspection PyUnresolvedReferences
 import pygame
 
-from constants import PLAYER_TURN_SPEED, PLAYER_SPEED, SHOT_RADIUS, PLAYER_SHOOT_SPEED
+from constants import PLAYER_TURN_SPEED, PLAYER_SPEED, SHOT_RADIUS, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN_SECONDS
 from circleshape import CircleShape
 from constants import PLAYER_RADIUS, LINE_WIDTH
 from shot import Shot
@@ -11,6 +11,7 @@ class Player(CircleShape):
     def __init__(self,x,y):
         super().__init__(x,y,PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_cd = 0
 
     # in the Player class
     def triangle(self):
@@ -37,12 +38,17 @@ class Player(CircleShape):
         self.position += rotated_with_speed_vector
 
     def shoot(self):
-        shot = Shot(self.position[0],self.position[1],SHOT_RADIUS)
-        shot.velocity = pygame.Vector2(0,1).rotate(self.rotation)*PLAYER_SHOOT_SPEED
+        if self.shoot_cd > 0:
+            return
+        else:
+            self.shoot_cd = PLAYER_SHOOT_COOLDOWN_SECONDS
+            shot = Shot(self.position[0],self.position[1],SHOT_RADIUS)
+            shot.velocity = pygame.Vector2(0,1).rotate(self.rotation)*PLAYER_SHOOT_SPEED
 
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.shoot_cd -= dt
 
         if keys[pygame.K_a]:
             self.rotate(dt*(-1))
